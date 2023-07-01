4878251b6ccc7204c087af998a3d5cfc53aac447
Nooorrrr <noor.bnhdd@gmail.com>
2023-07-01 14:16:58 +0100

    Act 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
+    a=string;
+    if(number>0){
+    for (let i=0;i<number-1;i++){
+    a+=string;
+        ;}
+        return a;
+    } else if (number==''){
+        return '';
+    }else {
+        return 'ERROR';
+    }
 
-};
+}
 
 // Do not edit below this line
 module.exports = repeatString;
