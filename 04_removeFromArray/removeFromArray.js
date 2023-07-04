23eb80466c16e3206a1d94558ff9daceeee62b37
Nooorrrr <noor.bnhdd@gmail.com>
2023-07-04 15:58:42 +0100

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...nbr) {
+    const newArray=[];
+    let size=arr.length;
+    for (let i=0;i<size;i++){
+        if (!nbr.includes(arr[i])){
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
